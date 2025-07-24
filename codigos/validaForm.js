2b11d75d1475de704adfb1bff517f3bc11f5b0bc
Gustavo Tamanini <gustavotamanini2005@gmail.com>
2025-07-24 17:42:21 -0300

    Validação de campos vazios. JS.

---
+++ codigos/validaForm.js
@@ -0,0 +1,32 @@
+formulario = document.querySelector('#formulario');
+cxEmail = document.querySelector('#email');
+cxSenha = document.querySelector('#senha');
+cxErroEmail = document.querySelector('#erro-email')
+cxErroSenha = document.querySelector('#erro-senha');
+
+cxEmail.addEventListener('focusout', function(e){
+    validarEmail();
+})
+
+cxSenha.addEventListener('focusout', function(e){
+    validarSenha();
+    
+})
+
+function validarEmail(){
+    email = cxEmail.value;
+    cxErroEmail.style.display = validarCampoVazio(email);
+}
+
+function validarSenha(){
+    senha = cxSenha.value;
+    cxErroSenha.style.display = validarCampoVazio(senha);
+}
+
+function validarCampoVazio(campo){
+    if(campo == ""){
+        return "block";
+    }else{
+        return "none";
+    }
+}
